cc7eadf9ad5b6592ff93d4f05f4daacbdf71a0ef
sal <sal@sal-H81-Linux>
2016-10-30 10:45:02 +0700

    Pushing all

---
+++ src/Action/Sell/add.java
@@ -0,0 +1,11 @@
+package Action.Sell;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class add extends ActionSupport{
+@Override
+public String execute() throws Exception {
+	// TODO Auto-generated method stub
+	return "success";
+}
+}
